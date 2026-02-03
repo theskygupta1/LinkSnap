cea0c68a6063f482a4fc0bb5d01150487fb1861b
Akash Kumar <akashkumar3429@gmail.com>
2026-02-03 07:26:30 +0530

    Added Base62 encoding for realistic short URLs

--- main.cpp
+++ main.cpp
@@ -1,8 +1,24 @@
+#include<algorithm>
 #include<iostream>
 #include<unordered_map>
 #include<string>
 
 using namespace std;
+
+const string BASE62 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+string encodeBase62(long long num) {
+    if (num == 0) return string(1, BASE62[0]);
+
+    string result;
+    while (num > 0) {
+        result.push_back(BASE62[num % 62]);
+        num /= 62;
+    }
+    reverse(result.begin(), result.end());
+    return result;
+}
+
 
 class URLShortener{
     private:
@@ -13,7 +29,8 @@ class URLShortener{
             counter = 1;
         }
         string shortenURL(const string& longURL){
-            string shortCode = "url" + to_string(counter++);
+            long long id = counter++;
+            string shortCode = encodeBase62(id);
             shortToLong[shortCode] = longURL;
             return shortCode;
         }
@@ -26,8 +43,10 @@ class URLShortener{
 
 int main(){
     URLShortener service;
-    string short1 = service.shortenURL("https://www.google.com");
-    string short2 = service.shortenURL("https://www.github.com");
-    cout << short1 << "->" << service.redirectURL(short1) << endl;
-    cout << short2 << "->" << service.redirectURL(short2) << endl;
+    string s1 = service.shortenURL("https://www.google.com");
+    string s2 = service.shortenURL("https://www.github.com");
+    string s3 = service.shortenURL("https://www.openai.com");
+    cout << s1 << "->" << service.redirectURL(s1) << endl;
+    cout << s2 << "->" << service.redirectURL(s2) << endl;
+    cout << s3 << "->" << service.redirectURL(s3) << endl;
 }
